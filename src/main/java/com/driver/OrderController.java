877789ca1e3fd8e5fe3221ddc3afd66571d23fe6
unknown <architasinghcse@gmail.com>
2023-02-25 14:57:09 +0530

    amazoff

--- src/main/java/com/driver/OrderController.java
+++ src/main/java/com/driver/OrderController.java
@@ -21,7 +21,7 @@ public class OrderController {
 
 
     @PostMapping("/add-order")
-    public ResponseEntity<String> addOrder(@RequestBody Order order){
+   public ResponseEntity<String> addOrder(@RequestBody Order order){
 
         return new ResponseEntity<>("New order added successfully", HttpStatus.CREATED);
     }
